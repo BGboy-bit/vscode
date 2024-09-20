1073d0839802d00f90255505fc8299582236cefb
BGboy-bit <2827354494@qq.com>
2024-09-20 14:29:45 +0800

    2024/09/20

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,37 +1,8 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
+#include<bits/stdc++.h>
 using namespace std;
-int x[12],sg[1050];
-bool vis[1050];
-void init(){
-	for(int i=0;i<11;i++){
-		x[i]=1<<i;
-	}
-	sg[0]=0;
-	for(int i=1;i<1002;i++){
-		memset(vis,0,sizeof(vis));
-		for(int j=0;x[j]<=i;j++){
-			vis[sg[i-x[j]]]=true;
-		}
-		for(int j=0;;j++){
-			if(!vis[j]){
-				sg[i]=j;
-				break;
-			}
-		}
-	}
-	return ;
+typedef __int128 i128;
+
+signed main() {
+    i128 i = 1;
+    return 0;
 }
-int main(){
-	int n;
-	init();
-    for(int i = 0; i <= 100; i ++ ) {
-        cout << i << " " << sg[i] << "\n";
-    }
-	// while(~scanf("%d",&n)){
-	// 	if(sg[n])printf("Kiki\n");
-	// 	else printf("Cici\n");
-	// }
-	return 0;
-} 
